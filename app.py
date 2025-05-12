b187a2b3fb4add5e00bf915029ce01b9e94d2f79
Ayush Mangy <92621603+ayushm-01@users.noreply.github.com>
2025-05-12 11:43:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -787,7 +787,7 @@ def main():
                         elif predicted_class == 2:
                                 st.info("Class 2: This mutation is likely pathogenic. Further clinical correlation is advised.")
 
-                       elif predicted_class == 3:
+                        elif predicted_class == 3:
                                 st.info("Class 3: Functional alteration variant. This mutation is known to alter the protein structure or function, potentially leading to changes in cellular processes. While it is not directly linked to disease causation, its impact on gene function may influence disease progression or treatment response in some individuals.")
 
 
